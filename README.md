b958b1fcaa3c45fdf698970875c3804cf1f6ebea
Yuto Takakura <yutotakakura0320@gmail.com>
2023-01-02 18:18:04 +0900

    APIにバージョンをつけてグループ化

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # 参考文献
 - [ginを最速でマスターしよう](https://qiita.com/Syoitu/items/8e7e3215fb7ac9dabc3a)
-- [[Golang/Go言語]GinフレームワークでAPI開発2~CRUDの実装~](https://selfnote.work/20211120/programming/golang-gin-api-2/)
-- [[Golang/Go言語]GinフレームワークでAPI開発3~OpenAPIの導入~](https://selfnote.work/20211125/programming/golang-gin-api-3/)
+- [[Golang/Go言語]GinフレームワークでAPI開発1~Hello アプリ~](https://selfnote.work/20211115/programming/golang-gin-api-1/)
+- [[Golang/Go言語]GinフレームワークでAPI開発2 CRUDの実装](https://selfnote.work/20211120/programming/golang-gin-api-2/)
+- [[Golang/Go言語]GinフレームワークでAPI開発3 OpenAPIの導入](https://selfnote.work/20211125/programming/golang-gin-api-3/)
--- routes/route.go
+++ routes/route.go
@@ -8,15 +8,18 @@ import (
 
 func NewRoutes() *gin.Engine {
 	router := gin.Default()
-	// レシピ作成
-	router.POST("/recipes", handlers.NewRecipeHandler)
-	// レシピ一覧の取得
-	router.GET("/recipes", handlers.ListRecipesHandler)
-	// レシピの更新
-	router.PUT("/recipes/:id", handlers.UpdateRecipeHandler)
-	// レシピの削除
-	router.DELETE("/recipes/:id", handlers.DeleteRecipeHandler)
-	// レシピの検索
-	router.GET("/recipes/search", handlers.SearchRecipesHandler)
-	return router
+	v1 := router.Group("/v1")
+	{
+		// レシピ作成
+		v1.POST("/recipes", handlers.NewRecipeHandler)
+		// レシピ一覧の取得
+		v1.GET("/recipes", handlers.ListRecipesHandler)
+		// レシピの更新
+		v1.PUT("/recipes/:id", handlers.UpdateRecipeHandler)
+		// レシピの削除
+		v1.DELETE("/recipes/:id", handlers.DeleteRecipeHandler)
+		// レシピの検索
+		v1.GET("/recipes/search", handlers.SearchRecipesHandler)
+		return router
+	}
 }
