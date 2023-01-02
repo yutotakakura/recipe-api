f1428ed728cd5dcc62d5e7908bae5b14dca59252
Yuto Takakura <yutotakakura0320@gmail.com>
2023-01-02 17:39:25 +0900

    Modelを定義

---
+++ models/recipe.go
@@ -0,0 +1,12 @@
+package models
+
+import "time"
+
+type Recipe struct {
+	ID           string    `json:"id"`
+	Name         string    `json:"name"`
+	Tags         []string  `json:"tags"`
+	Ingredients  []string  `json:"ingredients"`
+	Instructions []string  `json:"instructions"`
+	PublishedAt  time.Time `json:"publishedAt"`
+}
